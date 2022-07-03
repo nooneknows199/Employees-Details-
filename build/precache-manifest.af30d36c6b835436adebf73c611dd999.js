1a738a6439791d9cfc6cccf62963cd05cbb08939
unknown <nooneknows199>
2022-07-04 01:52:56 +0530

    babel error resolved

---
+++ build/precache-manifest.af30d36c6b835436adebf73c611dd999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76132d57634c6c4bedd7c616a617eac3",
+    "url": "/Employees-Details-/index.html"
+  },
+  {
+    "revision": "98f04af84a173fb40805",
+    "url": "/Employees-Details-/static/css/main.f9a09e2d.chunk.css"
+  },
+  {
+    "revision": "a5948c2b0b85bb9f1763",
+    "url": "/Employees-Details-/static/js/2.1ff8bfe2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Employees-Details-/static/js/2.1ff8bfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f04af84a173fb40805",
+    "url": "/Employees-Details-/static/js/main.652b6c3b.chunk.js"
+  },
+  {
+    "revision": "780bc1d855cd6ccb231b",
+    "url": "/Employees-Details-/static/js/runtime-main.138b2082.js"
+  }
+]);
